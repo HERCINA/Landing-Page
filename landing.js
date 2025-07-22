303e51e89b070f028b7d26ebbe6dab185b4fe9be
HercinaDJOB <hercinadjob@gmail.com>
2025-07-22 22:50:51 +0100

    adding t css

--- landing.js
+++ landing.js
@@ -17,19 +17,21 @@ function nextTestimonial() {
 //questions
 
 document.querySelectorAll('.faq-question').forEach(question => {
-  question.addEventListener('click', () => {
-    const currentItem = question.closest('.faq-item');
+    question.addEventListener('click', () => {
+      const faqItem = question.closest('.faq-item');
 
-    //close the order items
-    document.querySelectorAll('.faq-item').forEach(item => {
-      if (item !== currentItem) {
-        item.classList.remove('open');
-      }
+      // Close other FAQs
+      document.querySelectorAll('.faq-item').forEach(item => {
+        if (item !== faqItem) {
+          item.querySelector('.faq-answer').classList.add('hidden');
+          item.querySelector('.arrow-icon').classList.remove('rotate-180');
+        }
+      });
+
+      // Toggle current FAQ
+      faqItem.querySelector('.faq-answer').classList.toggle('hidden');
+      faqItem.querySelector('.arrow-icon').classList.toggle('rotate-180');
     });
-
-    // close the previous answer
-    currentItem.classList.toggle('open');
   });
-});
 
     
