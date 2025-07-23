04ee9a32f65a1e8c9ab303160a9946d379c0b223
HercinaDJOB <hercinadjob@gmail.com>
2025-07-23 10:20:36 +0100

    quelques changement

--- landing.js
+++ landing.js
@@ -17,21 +17,17 @@ function nextTestimonial() {
 //questions
 
 document.querySelectorAll('.faq-question').forEach(question => {
-    question.addEventListener('click', () => {
-      const faqItem = question.closest('.faq-item');
+  question.addEventListener('click', () => {
+    const currentItem = question.closest('.faq-item');
 
-      // Close other FAQs
-      document.querySelectorAll('.faq-item').forEach(item => {
-        if (item !== faqItem) {
-          item.querySelector('.faq-answer').classList.add('hidden');
-          item.querySelector('.arrow-icon').classList.remove('rotate-180');
-        }
-      });
-
-      // Toggle current FAQ
-      faqItem.querySelector('.faq-answer').classList.toggle('hidden');
-      faqItem.querySelector('.arrow-icon').classList.toggle('rotate-180');
+    //close the order items
+    document.querySelectorAll('.faq-item').forEach(item => {
+      if (item !== currentItem) {
+        item.classList.remove('open');
+      }
     });
-  });
 
-    
+    // close the previous answer
+    currentItem.classList.toggle('open');
+  });
+})
